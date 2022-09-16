24e6fc6b3bc5e63396e3d82af4e1e629dd76b9da
Yasz Avellia <zakamaragames@gmail.com>
2022-09-16 10:05:06 +0700

    delete succes

--- edit.php
+++ edit.php
@@ -4,7 +4,7 @@ $conn = mysqli_connect('localhost', 'root', '', 'crudlog');
 
 $id = $_GET['id'];
 
-$datas = $conn->query("SELECT * FROM anggota");
+$datas = $conn->query("SELECT * FROM anggota WHERE id = '$id'");
 
 $data = mysqli_fetch_assoc($datas);
 
@@ -14,7 +14,21 @@ if(isset($_POST['submit'])){
     $telepon = htmlspecialchars($_POST['telepon']);
     $alamat = htmlspecialchars($_POST['alamat']);
 
-    $conn->query("UPDATE anggota SET nama = '$nama', username = '$username', telepon = '$telepon', alamat = '$alamat' WHERE id = '$id'");
+    $update = $conn->query("UPDATE anggota SET nama = '$nama', username = '$username', telepon = '$telepon', alamat = '$alamat' WHERE id = '$id'");
+    if($update){
+        if (isset($suc)) {
+            echo "<script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Berhasil Menyimpan Data',
+                showConfirmButton: false,
+                timer: 1500
+              })
+                </script>";
+        } else {
+            die;
+        }
+    }
 }
 
 
@@ -45,23 +59,24 @@ if(isset($_POST['submit'])){
                             </div>
                             <div class="mb-3">
                                 <label for="nama">Nama</label>
-                                <input autocomplete="off" type="text" required name="nama" placeholder="" class="form-control" id="nama">
+                                <input autocomplete="off" type="text" required name="nama" placeholder="" value="<?= $data['nama'] ?>" class="form-control" id="nama">
                             </div>
                             <div class="mb-3">
                                 <label for="username">Username</label>
-                                <input type="text" autocomplete="off" name="username" required placeholder="" class="form-control" id="username">
+                                <input type="text" autocomplete="off" value="<?= $data['username'] ?>" name="username" required placeholder="" class="form-control" id="username">
                             </div>
                             <div class="mb-3">
                                 <label for="telp">No Telp</label>
-                                <input type="number" name="telepon" autocomplete="off" required placeholder=""  class="form-control" id="telp">
+                                <input type="number" value="<?= $data['telepon'] ?>" name="telepon" autocomplete="off" required placeholder=""  class="form-control" id="telp">
                             </div>
                             <div class="mb-3">
                                 <label for="alamat">Alamat</label>
-                                <input type="text" name="alamat" required autocomplete="off" placeholder="" class="form-control" id="alamat">
+                                <input type="text" value="<?= $data['alamat'] ?>" name="alamat" required autocomplete="off" placeholder="" class="form-control" id="alamat">
                             </div>
                             <div class="footer">
                                 <button class="btn btn-primary" name="submit" type="submit">Submit</button>
                                 <button class="btn btn-danger" type="reset">Reset</button>
+                                <a href="index.php" class="btn btn-info">Back</a>
                             </div>
                         </form>
                     </div>
@@ -70,6 +85,20 @@ if(isset($_POST['submit'])){
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <?php 
+    
+    if (isset($suc)) {
+        echo "<script>
+        Swal.fire({
+            icon: 'success',
+            title: 'Berhasil Mengubah Data',
+            showConfirmButton: false,
+            timer: 1500
+          })
+            </script>";
+    }
+
+    ?>
 </body>
 
 </html>
